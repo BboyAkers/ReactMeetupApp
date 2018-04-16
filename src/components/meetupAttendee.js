60f7f6bdfb9093deecdbc807eddf2955088bf283
Austin Akers <austin.akers5@gmail.com>
2018-04-16 11:34:11 -0500

    eliminating pseudo code

--- src/components/meetupAttendee.js
+++ src/components/meetupAttendee.js
@@ -9,14 +9,6 @@ class MeetupAttendee extends Component {
         <div>
           <img className="thumbnail-image" src={this.props.attendeeData.member.photo.thumb_link} alt="thumbnail image"/>
           <h4 className="display-inline-block attendee-name">{this.props.attendeeData.member.name}</h4>
-          
-          {/* <h6>{this.props.event.local_date}</h6> */}
-        </div>
-        <div>
-          {/* <h5>{this.props.event.venue.name}</h5>
-          <p>{this.props.event.venue.address_1}</p>
-          <p>{this.props.event.venue.city}</p>
-          <p>{this.props.event.venue.state}</p> */}
         </div>
         <br/>
         <hr />
