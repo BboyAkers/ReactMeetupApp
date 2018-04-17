54d8151b1e4e20a02251419e21503688c069c268
Joe Warren <joe.warren7@gmail.com>
2018-04-16 22:04:22 -0500

    refactor: add React inline styles, section comments, and destructured state & props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+// External Dependencies
 import React, { Component } from 'react';
-import './styles/App.css';
-
-//Dependencies
 import jsonp from 'jsonp-es6';
 
+// Internal Dependencies
 import MeetupList from './components/meetupList';
 
+// Local Variables
+const wrapper = {
+  background: '#e2e1e0',
+  textAlign: 'center'
+}
+
+// Component Definition
 class App extends Component {
   state = {
     events: []
@@ -13,17 +19,18 @@ class App extends Component {
   componentDidMount() {
     let API_KEY = '13f4a733f524e2a2e1e334637186651';
     jsonp(`https://api.meetup.com/reactjs-dallas/events?key=${API_KEY}&sign=true&photo-host=public&page=20`)
-    .then(res => {
-      const events = res.data;
-      this.setState({ events });
-      console.log(events)
-    })
+      .then(res => {
+        const events = res.data;
+        this.setState({ events });
+        console.log(events)
+      })
   }
 
   render() {
+    const { events } = this.state;
     return (
-      <div className="App">
-        <MeetupList events={this.state.events} />
+      <div style={wrapper}>
+        <MeetupList events={events} />
       </div>
     );
   }
--- src/components/meetupCard.js
+++ src/components/meetupCard.js
@@ -1,30 +1,42 @@
+// External Dependencies
 import React, { Component } from 'react';
 import MeetupRsvpList from './meetupRsvpList'
 
+// Internal Dependencies
 import '../styles/Card.css';
 
-class MeetupCard extends Component {
+// Local Variables
+const cardWrapper = {
+  padding: '1.0em',
+  textAlign: 'left',
+  background: '#fff',
+  borderRadius: '2px',
+  display: 'inline-block',
+  margin: '1rem',
+  position: 'relative'
+}
+// Component Definition
+export default class MeetupCard extends Component {
   displayDescription() {
-    return {__html: this.props.event.description};
+    return { __html: this.props.event.description };
   }
   render() {
+    const { event } = this.props;
     return (
-      <div className="card card-depth-1">
+      <div style={cardWrapper} className="card-depth-1">
         <div>
-          <h4>{this.props.event.name}</h4>
-          <h6>{this.props.event.local_date}</h6>
+          <h4>{event.name}</h4>
+          <h6>{event.local_date}</h6>
           <p dangerouslySetInnerHTML={this.displayDescription()} />
         </div>
         <div>
-          <h5>{this.props.event.venue.name}</h5>
-          <p>{this.props.event.venue.address_1}</p>
-          <p>{this.props.event.venue.city}</p>
-          <p>{this.props.event.venue.state}</p>
+          <h5>{event.venue.name}</h5>
+          <p>{event.venue.address_1}</p>
+          <p>{event.venue.city}</p>
+          <p>{event.venue.state}</p>
         </div>
-        <MeetupRsvpList eventId={this.props.event.id} />
+        <MeetupRsvpList eventId={event.id} />
       </div>
     );
   }
 }
-
-export default MeetupCard;
